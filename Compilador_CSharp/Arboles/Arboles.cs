d545a1bca1cd9908b7c91fb6f3315bb177aa287b
Martin F. Hurtado <martindrumer21@gmail.com>
2019-12-30 20:15:17 -0700

    Avance en Arboles.cs
    Enumeradores agregados y clase NodoArbol listo.

--- Compilador_CSharp/Arboles/Arboles.cs
+++ Compilador_CSharp/Arboles/Arboles.cs
@@ -1,13 +1,61 @@
-using System.Collections.Generic;
-using System.IO;
-
 namespace Compilador_CSharp
 {
+    #region Enumeradores
+    public enum tipoExpresion
+    {
+        Aritmetica,
+        Comparacion,
+        Identificador,
+        Constante,
+        Cadena,
+        Vacio
+    }
+    public enum tipoValor
+    {
+        Entero,
+        Flotante,
+        Booleano,
+        Cadena,
+        Vacio
+    }
+    public enum tipoSentencia
+    {
+        Asignacion,
+        If,
+        For,
+        Incremento,
+        Vacio
+    }
+    public enum operaciones
+    {
+        suma,
+        resta,
+        multiplicacion,
+        division,
+        vacio
+    }
+    #endregion
+
 	public class NodoArbol
-	{
-		public NodoArbol hijoIzquierdo;
+    {
+        public NodoArbol hermano;
+        public NodoArbol hijoIzquierdo;
         public NodoArbol hijoCentro;
         public NodoArbol hijoDerecho;
-        public NodoArbol Hermano;
-	}
+
+        public string lexema;
+        public string operador;
+        public tipoExpresion miTipoExpresion;
+        public tipoSentencia miTipoSentencia;
+        public tipoValor miTipoValor;
+        public operaciones miTipoOperacion;
+        public tipoValor tipoValorHijoIzquierdo;
+        public tipoValor tipoValorHijoDerecho;
+    }
+
+    public class ArbolSintactico
+    {
+
+    }
+
 }
